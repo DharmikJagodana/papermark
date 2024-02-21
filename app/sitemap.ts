c2507bbe23f1ef458c16b4df1318c062897e65bb
Marc Seitz <marc@mfts.io>
2024-02-21 15:22:12 +1100

    feat: add investors to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,62 +1,70 @@
 import { getPosts } from "@/lib/content/blog";
+import { getInvestors } from "@/lib/content/investor";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const posts = await getPosts()
+  const posts = await getPosts();
   const blogs = posts.map((post) => ({
     url: `https://www.papermark.io/blog/${post?.data.slug}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
-  
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
+
+  const investors = await getInvestors();
+  const investorsUrls = investors.map((investor) => ({
+    url: `https://www.papermark.io/investors/${investor.slug}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
+
   return [
     {
       url: "https://www.papermark.io",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/alternatives/docsend",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/alternatives/brieflink",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/alternatives/pandadoc",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/alternatives/google-drive",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/alternatives/pitch",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/privacy",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/open-source-investors",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/docsend-alternatives",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/oss-friends",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://vc.papermark.io",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.papermark.io/investors",
-      lastModified: new Date().toISOString().split('T')[0],
+      lastModified: new Date().toISOString().split("T")[0],
     },
-    ...blogs
+    ...blogs,
+    ...investorsUrls,
   ];
 }
